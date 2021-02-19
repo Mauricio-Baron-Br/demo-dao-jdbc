cdb39ae460e35c5b0d10ed6d25a26877030dfda8
Mauricio Baron <mauriciobaron2014@gmail.com>
2021-02-19 19:17:17 -0300

    implementation the seller does not exist id

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,7 +35,7 @@ public class Program {
 		
 		System.out.println("\n=== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		System.out.println("\n=== TEST 5: seller Update =====");
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -97,7 +97,10 @@ public class SellerDaoJDBC implements SellerDao {
 		try {
 			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 			st.setInt(1, id);
-			st.executeUpdate();
+			int rows = st.executeUpdate();
+			if(rows == 0) {
+				throw new DbException("The seller does not exist");
+			}
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
